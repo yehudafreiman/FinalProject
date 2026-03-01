b244df2aaea4383251db2d626b3afe4f125ab217
yehudafreiman <yehfre@icloud.com>
2026-03-01 11:55:22 +0200

    init files

---
+++ consumer/consumer.py
@@ -0,0 +1,15 @@
+class Consumer:
+    def __init__(self):
+        pass
+
+    def listen_to_kafka(self):
+        pass
+
+    def create_unique_id(self):
+        pass
+
+    def send_metadata_to_elasticsearch(self):
+        pass
+
+    def send_file_to_mongodb(self):
+        pass
---
+++ producer/producer.py
@@ -0,0 +1,14 @@
+class Producer:
+    def __init__(self):
+        pass
+
+    def read_files(self):
+        pass
+
+    def create_metadata(self):
+        pass
+
+    def send_to_kafka(self):
+        pass
+
+
