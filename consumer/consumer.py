e1a24286bd0d30fc826deada3ed0b46ffd5c3db5
yehudafreiman <yehfre@icloud.com>
2026-03-02 13:37:38 +0200

    implement stt

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -3,6 +3,7 @@ from kafka import KafkaConsumer
 from json import loads
 from connection import fs, es
 from logger import Logger
+import speech_recognition as sr
 
 logger = Logger.get_logger()
 
@@ -15,6 +16,7 @@ class Consumer:
         value_deserializer=lambda x: loads(x.decode('utf-8')))
         self.mongo_connection = fs
         self.elasticsearch_connection = es
+        self.recognizer = sr.Recognizer()
 
     def listen_to_kafka(self):
         for message in self.consumer:
@@ -27,16 +29,25 @@ class Consumer:
         data["unique_id"] = uuid.uuid4()
         logger.info("create unique id")
 
+    def speach_to_text(self):
+        data = self.listen_to_kafka()
+        with sr.AudioFile(data['file path']) as source:
+            audio_data = self.recognizer.record(source)
+        context = self.recognizer.listen(audio_data)
+        return context
+
     def send_metadata_to_elasticsearch(self):
         data = self.listen_to_kafka()
+        context = self.speach_to_text()
         self.elasticsearch_connection.index(
             index='test',
             document={
+                'context': context,
                 'character': data
             })
         logger.info("send metadata to elasticsearch")
 
     def send_file_to_mongodb(self):
         data = self.listen_to_kafka()
-        self.mongo_connection.put(data)
+        self.mongo_connection.put(data['file path'])
         logger.info("send file to mongodb")
