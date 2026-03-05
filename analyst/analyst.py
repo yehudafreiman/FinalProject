d210853e6d2cdb20e240f6a4fc1b8112d52844fc
yehudafreiman <yehfre@icloud.com>
2026-03-05 13:41:51 +0200

    init analyst

---
+++ analyst/analyst.py
@@ -0,0 +1,13 @@
+import base64
+
+def encode_list(base64_string):
+    base64_bytes = base64_string.encode("ascii")
+    sample_string_bytes = base64.b64decode(base64_bytes)
+    sample_string = sample_string_bytes.decode("ascii")
+    result = sample_string.split(',')
+    return result
+
+encode_list("R2Vub2NpZGUsV2FyIENyaW1lcyxBcGFydGhlaWQsTWFzc2FjcmUsTmFrYmEsRGlzcGxhY2VtZW50LEh1bWFuaXRhcmlhbiBDcmlzaXMsQmxvY2thZGUsT2NjdXBhdGlvbixSZWZ1Z2VlcyxJQ0MsQkRT")
+encode_list("RnJlZWRvbSBGbG90aWxsYSxSZXNpc3RhbmNlLExpYmVyYXRpb24sRnJlZSBQYWxlc3RpbmUsR2F6YSxDZWFzZWZpcmUsUHJvdGVzdCxVTlJXQQ==")
+
+
---
+++ analyst/logger.py
@@ -0,0 +1,33 @@
+import logging
+from connection import es
+from datetime import datetime
+
+class Logger:
+    _logger = None
+    @classmethod
+    def get_logger(cls, name="your_logger_name", es_host="https://localhost:9200",
+                   index="your_index_logs_name", level=logging.DEBUG):
+        if cls._logger:
+            return cls._logger
+        logger = logging.getLogger(name)
+        logger.setLevel(level)
+        if not logger.handlers:
+            class ESHandler(logging.Handler):
+                def emit(self, record):
+                    try:
+                        es.index(index=index, document={
+                            "timestamp": datetime.utcnow().isoformat(),
+                            "level": record.levelname,
+                            "logger": record.name,
+                            "message": record.getMessage()
+                        })
+                    except Exception as e:
+                        print(f"ES log failed: {e}")
+            logger.addHandler(ESHandler())
+            logger.addHandler(logging.StreamHandler())
+        cls._logger = logger
+        return logger
+
+
+
+
